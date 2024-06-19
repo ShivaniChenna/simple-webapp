f733c2cb6d2066fe255e8619ae82855909b6c7f4
Shivani Chenna <94120892+ShivaniChenna@users.noreply.github.com>
2024-06-19 12:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "This is Shivani :)"
+    return "This is Shivani...:)"
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
